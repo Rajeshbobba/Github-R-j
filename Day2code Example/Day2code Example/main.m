f9790dfc95160f9e4796fece6f2c9f41267b41d8
Rajeshbobba <bobbarajesh@yahoo.com>
2014-05-06 16:05:08 -0400

    Day2 2nd prog
    changes 2nd prog

--- Day2code Example/Day2code Example/main.m
+++ Day2code Example/Day2code Example/main.m
@@ -7,16 +7,48 @@
 //
 
 #import <Foundation/Foundation.h>
+// @interface section
+@interface Fraction :NSObject
 
+-(void)Print;
+-(void)setNumerator:(int)n;
+_(void)setDenominator:(int)d;
+
+@end
+// @implementation section
+
+{
+    int numerator;
+    int denominator;
+}
+_(void)Print
+{
+    NSLog(@"%i/%i",numerator,denominator);
+}
+_(void)setNumerator:(int)n
+{
+    numerator =n;
+}
+-(void)setDenominator:(int)d
+{
+    denominator =d;
+}
+@end
+// Program section
 int main(int argc, const char * argv[])
 {
-
     @autoreleasepool {
-        int numerator=1;
-        int denominator=3;
-        NSLog(@"The fration is %i/%i",numerator,denominator);
+        Fraction *myFraction;
+        // Create an instance of a Fraction
+        my Fraction = [Fraction alloc];
+        my Fraction = [myFraction init];
+        
+        [myFraction setNumerator:1];
+        NSLog[@'the value of the fraction is"'];
+        return print;
         
     }
     return 0;
+    
 }
 
