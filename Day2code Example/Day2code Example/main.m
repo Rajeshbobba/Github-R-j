33df05b7311ae557a335edcd53dfc09f68ab43f7
Rajeshbobba <bobbarajesh@yahoo.com>
2014-05-06 14:00:41 -0400

    Day2 Class

---
+++ Day2code Example/Day2code Example/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Day2code Example
+//
+//  Created by User1 on 2014-05-06.
+//  Copyright (c) 2014 Rajesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
